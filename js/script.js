9e0d008aa1018d4a166b1d0699c0bd7990af9284
SiRiuS-040 <79222154+SiRiuS-040@users.noreply.github.com>
2021-06-25 14:27:51 +0300

    Завершение вёрстки главной страницы

---
+++ js/script.js
@@ -0,0 +1,30 @@
+function closeForm() {
+   var modal = document.getElementById('FormOpen');
+   modal.style.display = 'none';
+}
+
+function showForm() {
+   var modal = document.getElementById('FormOpen');
+   modal.style.display = 'block';
+}
+
+function closeCart() {
+   var modal = document.getElementById('GoodsInCart');
+   modal.style.display = 'none';
+}
+
+function showCart() {
+   var modal = document.getElementById('GoodsInCart');
+   modal.style.display = 'block';
+}
+
+function closeMap() {
+   var modal = document.getElementById('MapPopUp');
+   modal.style.display = 'none';
+}
+
+function showMap() {
+   var modal = document.getElementById('MapPopUp');
+   modal.style.display = 'block';
+}
+
